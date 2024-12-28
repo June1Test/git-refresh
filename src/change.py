43c0b35a4a83f044aa1cd3ec7d25afa969fc237d
June Test <junetest@Junes-Mac-Studio.local>
2024-12-28 14:41:05 -0500

    Version3

--- src/change.py
+++ src/change.py
@@ -1,2 +1,4 @@
 print("adding a new change ")
 print("per my last email ,Venezuela will not be on the new year export")
+print("Instead we will be adding Nicaragua and Bolivia")
+print("europe regulation is what we will need to focus on the first quater")
