ded6f1d0e7448279d8bcbd63ef228dc55b060144
June Test <junetest@Junes-Mac-Studio.local>
2024-12-28 14:33:27 -0500

    Version 2

--- src/change.py
+++ src/change.py
@@ -1,1 +1,2 @@
 print("adding a new change ")
+print("per my last email ,Venezuela will not be on the new year export")
--- src/index.py
+++ src/index.py
@@ -1,3 +1,3 @@
 print("this is the index file")
 
-print("why are you in my face")
+print("why are you in my face version 2")
